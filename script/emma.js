919087c126218e204d4e49432672f73f853b4f29
Emma Eriksson <emmae-91@hotmail.com>
2021-11-05 23:14:42 +0100

    Lagt in kod för mitt fotografkort

--- script/emma.js
+++ script/emma.js
@@ -1,3 +1,58 @@
+function openModal(){
+  alert("hej");
+  document.getElementById("modal").style.display = "block";
+  document.getElementById("modal-image").style.display = "block";
+  document.getElementById("modal").style.backgroundColor = "black";
+}
+
+function openInfoModal(){
+  document.getElementById("info-modal").style.display = "block";
+  document.getElementById("card-info-id").style.display = "none";
+}
+
+function closeInfoModal(){
+  document.getElementById("info-modal").style.display = "none";
+  document.getElementById("card-info-id").style.display = "block";
+}
+
+window.onclick = function(event){
+  const modal = document.getElementById("info-modal");
+  const target = event.target;
+  if(target == modal){
+      closeInfoModal();
+  }
+}
+
+const naturalDisasterPictures = [
+  "../media/images/fire.jpg", 
+  "../media/images/floods.jpg", 
+  "../media/images/tornado.jpg", 
+  "../media/images/tsunami.jpg", 
+  "../media/images/wave.jpg",
+  "../media/images/vulcano.jpg"];
+let index = 0;
+const numOfPictures = naturalDisasterPictures.length;
+
+function nextPicture(){
+  if(index === numOfPictures - 1){
+      index = 0;
+  }else{
+      index++;
+  }
+  let naturalDisasterPicture = naturalDisasterPictures[index];
+  document.getElementById("natural-disaster-picture").src = naturalDisasterPicture;
+}
+
+function previousPicture(){
+  if(index === 0){
+      index = numOfPictures - 1;
+  }else{
+      index--;
+  }
+  let naturalDisasterPicture = naturalDisasterPictures[index];
+  document.getElementById("natural-disaster-picture").src = naturalDisasterPicture;
+}
+
 //Function for opening the Modal
 function openModal(){
     document.getElementById("modal-container").style.display = "block";
@@ -29,8 +84,7 @@ function minusSlide(){
 function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("modal-image");
-    let thumbnails = document.getElementsByClassName("modal-thumbnails");
-    let captionText = document.getElementById("caption");
+    let thumbnails = document.getElementsByClassName("thumbnail-image");
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
